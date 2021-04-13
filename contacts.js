37d3e9317f45d1199305c2b8649b5a5b4a004da0
moonzpwr <60944420+moonzpwr@users.noreply.github.com>
2021-04-13 14:18:35 +0300

    add async/await method

--- contacts.js
+++ contacts.js
@@ -1,3 +1,4 @@
+const { table } = require('console');
 const fs = require('fs')
 const { promises: fsPromise } = fs;
 const path = require('path')
@@ -5,61 +6,63 @@ const contactsPath = path.join(__dirname, 'db', 'contacts.json')
 const shortid = require('shortid');
 
 
-function listContacts() {
-    fsPromise.readFile(contactsPath)
-        .then((data) => {
-        console.table(JSON.parse(data))//выводим в консоль таблицу контактов
-        })
-        .catch((err) => {console.log(err);})//отлавливаем ошибки
+async function listContacts() {
+    try {
+        const data = await fsPromise.readFile(contactsPath)
+        console.table(JSON.parse(data))
+        return JSON.parse(data)
+    } catch (error) {
+        console.log(error);
+    }
 }
 
-function getContactById(contactId) {    
-    fsPromise.readFile(contactsPath)
-        .then((data) => {
-            const allContacts = JSON.parse(data.toString())//забираем все контакты
-            const result = allContacts.find(({ id }) => id.toString() === contactId)//ищем во всех контактах нужный
-            if (result === undefined) {
-                console.log('No such contact exists!');
+async function getContactById(contactId) {
+    try {
+        const data = await fsPromise.readFile(contactsPath)
+        const contact = JSON.parse(data).find(({ id }) => id.toString() === contactId)
+        if (contact === undefined) {
+             console.log('No such contact exists!');
                 return
-            }
-            console.table(result);//выводим в консоль нужный контакт
-        }).catch((err) => {console.error(err.message);})//отлавливаем ошибки
+        }
+        console.table(contact);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 async function removeContact(contactId) {
-  fsPromise.readFile(contactsPath)
-        .then((data) => {
-            const allContacts = JSON.parse(data)//забираем все контакты
-            const newList = allContacts.filter(({ id }) => id.toString() !== contactId);//делаем новый массив где удаляем нужный контакт
-            if (allContacts.find(({ id }) => id.toString() === contactId) === undefined){//проверка существует ли запрашиваемый контакт
+    try {
+        const data = await fsPromise.readFile(contactsPath)
+        const newList = JSON.parse(data).filter(({ id }) => id.toString() !== contactId)
+        if (JSON.parse(data).find(({ id }) => id.toString() === contactId) === undefined){//проверка существует ли запрашиваемый контакт
                 console.log('No such contact exists!');
                 return
-            }
-            fsPromise.writeFile(contactsPath, JSON.stringify(newList))//перезаписываем в файл новый масив
-            console.log(`Contact successfully deleted!`);
-            listContacts()//показываем обновленный список контактов
-
-        })
-        .catch((err) => { console.log(err); })//отлавливаем ошибки
+        }
+        await fsPromise.writeFile(contactsPath, JSON.stringify(newList))
+        console.log(`Contact successfully deleted!`);
+        await listContacts()
+    } catch (error) {
+       console.log(error); 
+    }
 }
 
 
-function addContact(name, email, phone) {
-  fsPromise.readFile(contactsPath)
-      .then((data) => {
-          const allContacts = JSON.parse(data)//забираем все контакты
-          const newContact = { id: shortid.generate(), name, email, phone }//генерируем новый контакт
-          if (allContacts.find(contact =>contact.name === name)) {//проверяем нет ли уже сонтакта с таким именем
+async function addContact(name, email, phone) {
+    try {
+        const data = await fsPromise.readFile(contactsPath)
+        const parseData = JSON.parse(data)
+        const newContact = { id: shortid.generate(), name, email, phone }
+        if (parseData.find(contact =>contact.name === name)) {//проверяем нет ли уже сонтакта с таким именем
                  console.log('Contact with this name alredy exist!');
                 return
-            }
-           allContacts.push(newContact)//добавляем новый контакт в список всех контактов 
-            fsPromise.writeFile(contactsPath, JSON.stringify(allContacts))//перезаписываем в файл новый масив
-          console.log(`Contact ${name} successfully added!`);//сообщение о том, что контакт успешно добавлен
-          listContacts()//показываем обновленный список контактов
-          
-        })
-        .catch((err) => { console.log(err); })//отлавливаем ошибки
+        }
+        parseData.push(newContact)
+        await fsPromise.writeFile(contactsPath, JSON.stringify(parseData))
+        console.log(`Contact ${name} successfully added!`);
+        await listContacts()
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 module.exports = {
